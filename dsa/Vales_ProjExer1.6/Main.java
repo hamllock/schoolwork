e8abe2346289f8d758d76646147a527fc284d3c2
BuildTools <unconfigured@null.spigotmc.org>
2023-10-04 13:38:38 +0800

    updated logic

--- dsa/Vales_ProjExer1.6/Main.java
+++ dsa/Vales_ProjExer1.6/Main.java
@@ -2,32 +2,21 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Stack<String> basevalue = new Stack<>();
 
         String[] arr = {
-                "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
-                "K", "L", "M", "N", "O", "P", "Q"
-        };
-
-        for (String x : arr) {
-            basevalue.push(x);
-        }
+                "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q" };
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Input: ");
+        System.out.print("Input: ");
         int input = sc.nextInt();
         sc.nextLine();
 
         while (true) {
-            System.out.println("Base: ");
+            System.out.print("Base: ");
             int base = sc.nextInt();
             sc.nextLine();
 
             if (base >= 2 && base <= 27) {
-                for (int i = 0; i < 27 - base; i++) {
-                    basevalue.pop();
-                }
 
                 int remainder;
                 Stack<Integer> stack = new Stack<>();
@@ -37,11 +26,12 @@ public class Main {
                     stack.push(remainder);
                 }
 
+                System.out.print("Output: ");
                 while (!stack.empty()) {
-                    String get = basevalue.get(stack.pop());
-                    System.out.print(get);
+                    System.out.print(arr[stack.pop()]);
                 }
-
+                
+                sc.close();
                 break;
             } else {
                 System.out.println("Invalid output, base must be between 2 - 27");
